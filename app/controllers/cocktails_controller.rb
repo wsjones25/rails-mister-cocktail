9b10c8267d1a15c2bcb598744b6c87e392f1130f
Will Jones <wsjones1101@gmail.com>
2018-05-17 12:51:08 +0100

    Rails new with frontend and form gems

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,14 @@
+class CocktailsController < ApplicationController
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+   def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+end
---
+++ app/models/dose.rb
@@ -0,0 +1,11 @@
+class Dose < ApplicationRecord
+  belongs_to :ingredient
+  belongs_to :cocktail
+
+  validates :description, presence: true
+  validates :cocktail, presence: true
+  validates :ingredient, presence: true
+  validates_uniqueness_of :cocktail_id, :scope => [:ingredient_id]
+end
+
+
